15c01082c0af06d68ce98b89158b3a002886e15f
shanyong <shanyong@kingsoft.com>
2021-11-17 14:09:06 +0800

    add single page pdf and double page pdf.

--- main.go
+++ main.go
@@ -6,18 +6,48 @@ import (
 	"github.com/jung-kurt/gofpdf"
 )
 
+const (
+	A4Width   = 595
+	A4Height  = 841.89
+	SideWidth = 10
+)
+
 func main() {
-	pdf := gofpdf.New("P", "pt", "A4", "")
+	// 输出单页版
+	SinglePage()
+
+	// 输出双页版
+	DoublePage()
+}
+
+// 单页输出
+func SinglePage() {
+	//规格：纵向、像素、A4
+	pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeA4, "")
+
 	for i := 1; i <= 388; i++ {
+
 		pdf.AddPage()
 		fileName := fmt.Sprintf("%08d.jpg", i)
-		pdf.Image(fileName, 0, 7, 596, 841.89, false, "", 0, "")
+		pdf.Image(fileName, SideWidth/2, 5, A4Width-SideWidth, A4Height, false, "", 0, "")
 	}
 
-	// pdf.AddPage()
-	// pdf.Image("00000001.jpg", 0, 14, 596, 841.89, false, "", 0, "")
-	// pdf.AddPage()
-	// pdf.Image("00000002.jpg", 0, 14, 596, 841.89, false, "", 0, "")
+	pdf.OutputFileAndClose("中国战争史地图集-单页.pdf")
+}
 
-	pdf.OutputFileAndClose("中国战争史地图集.pdf")
+// 双页输出
+func DoublePage() {
+	//规格：横向、像素、A3
+	pdf := gofpdf.New(gofpdf.OrientationLandscape, gofpdf.UnitPoint, gofpdf.PageSizeA3, "")
+
+	for i := 1; i <= 388/2; i++ {
+
+		pdf.AddPage()
+		fileName := fmt.Sprintf("%08d.jpg", 2*i-1)
+		pdf.Image(fileName, SideWidth/2, 5, A4Width-SideWidth, A4Height, false, "", 0, "")
+		fileName = fmt.Sprintf("%08d.jpg", 2*i)
+		pdf.Image(fileName, A4Width+SideWidth/2, 5, A4Width-SideWidth, A4Height, false, "", 0, "")
+	}
+
+	pdf.OutputFileAndClose("中国战争史地图集-双页.pdf")
 }
